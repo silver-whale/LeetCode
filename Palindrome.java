a90198343188899a096678696dc11c67300d7e4d
silver-whale <garlicwhale@gmail.com>
2022-03-18 14:42:42 +0900

    Day 3

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+class Palindrome {
+    public static boolean isPalindrome(int x) {
+        if(x<0) return false;
+        
+        int rev = 0;
+        int temp = x;
+        
+        while(temp>0){
+            rev += temp%10;
+            temp = temp/10;
+        }
+        
+        System.out.println(rev);
+        
+        if (rev==x) return true;
+        
+        return false;
+    }
+
+
+    public static void main(String[] args){
+        int value = 121;
+        isPalindrome(value);
+    }
+}
