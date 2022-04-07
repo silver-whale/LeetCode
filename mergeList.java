ff7e964b86158ad5c21e60ff7a9055207f23530e
silver-whale <garlicwhale@gmail.com>
2022-04-07 23:55:16 +0900

    Merge Two Sorted Lists

--- mergeList.java
+++ mergeList.java
@@ -9,9 +9,30 @@ public class mergeList {
      }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode mergeList = new ListNode();
+        ListNode head = new ListNode();
 
-        return mergeList;
+        // tail = pointer to last node
+        ListNode tail = head;
+
+        while(list1 != null && list2 != null){
+            // list1, list2 : address
+            if(list1.val < list2.val){
+                tail.next = list1;
+
+                list1 = list1.next;
+                tail = tail.next;
+            }
+            else{
+                tail.next = list2;
+
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+
+        tail.next = (list1 != null) ? list1 : list2;
+
+        return head.next;
     }
 
     public static void main(String[] args){
