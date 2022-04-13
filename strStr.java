5876161a15bc4fa48c6746f2a487755c5b4bef0b
silver-whale <garlicwhale@gmail.com>
2022-04-13 21:05:39 +0900

    strStr

---
+++ strStr.java
@@ -0,0 +1,15 @@
+public class strStr {
+    public static int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args){
+        String haystack = "aaaaa";
+        String needle = "bba";
+
+
+        System.out.println(strStr(haystack, needle));
+    }
+}
