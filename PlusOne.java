5ec5e94c43769b35ba6318c51a4eb24fc6b84cd6
silver-whale <garlicwhale@gmail.com>
2022-04-17 23:46:44 +0900

    Plus One

---
+++ PlusOne.java
@@ -0,0 +1,24 @@
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int flag = 0;
+        digits[digits.length-1] += 1;
+        for(int i = digits.length - 1; i >= 0; i--) {
+            digits[i] += flag;
+            if(digits[i] > 9) {
+                flag = 1;
+                digits[i] %= 10;
+            } else {
+                flag = 0;
+            }
+        }
+
+        if (flag==1){
+            digits = new int [digits.length+1];
+            digits[0] = 1;
+        }
+        return digits;
+    }
+    public static void main(String[] args){
+
+    }
+}
