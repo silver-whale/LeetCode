26875d324f254c5fa9de8528c9d870bd85283c77
silver-whale <garlicwhale@gmail.com>
2022-04-25 23:31:07 +0900

    Leetcode 144

--- Leetcode278.java
+++ Leetcode278.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Leetcode278 {
+public class Leetcode278 {
+    public boolean isBadVersion(int n){
+        // Abstract Function
+        return true;
+    }
+    public int firstBadVersion(int n) {
+        // list starts from 1
+        int left = 1, right = n;
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(isBadVersion(mid)){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
--- Leetcode704.java
+++ Leetcode704.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Leetcode704 {
+public class Leetcode704 {
+    public int search(int[] nums, int target){
+        if(nums.length == 1){
+            return nums[0] == target ? 0 : -1;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(end >= start){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
