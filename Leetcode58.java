a13587a002aeef75f06f12521ca5501121ac5301
silver-whale <garlicwhale@gmail.com>
2022-04-29 19:25:08 +0900

    Leetcode 58

---
+++ Leetcode58.java
@@ -0,0 +1,14 @@
+public class Leetcode58 {
+    public int lengthOfLastWord(String s) {
+        int index = s.length()-1;
+        int count = 0;
+        while(index>=0 && s.charAt(index) == ' '){
+            index -= 1;
+        }
+        while(index>=0 && s.charAt(index) != ' '){
+            count += 1;
+            index -= 1;
+        }
+        return count;
+    }
+}
