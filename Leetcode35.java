3c74477aae861ea7bd7f0c4c8968e683d83189f2
silver-whale <garlicwhale@gmail.com>
2022-05-17 23:57:33 +0900

    Leetcode 35

--- Leetcode35.java
+++ Leetcode35.java
@@ -1,21 +1,23 @@
 public class Leetcode35 {
     public int searchInsert(int[] nums, int target) {
-        if (nums.length == 1){
-            if (nums[0] == target){
+        if(nums.length == 1){
+            if(nums[0] == target){
                 return 0;
             }
             else{
-                return nums[0] >= target ? 0 : 1;
+                return nums[0]>=target ? 0 : 1;
             }
         }
 
-        int start = 0, end = nums.length - 1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(nums[mid]==target){
+        int start = 0, end = nums.length-1;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target){
                 return mid;
             }
-            else if (nums[mid]>target){
+            else if(nums[mid] > target){
                 end = mid - 1;
             }
             else{
