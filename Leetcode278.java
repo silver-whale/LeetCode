7d44808589dc97898cb9d3df1b26f491a6771bbf
silver-whale <garlicwhale@gmail.com>
2022-05-17 23:19:00 +0900

    Leetcode 278

--- Leetcode278.java
+++ Leetcode278.java
@@ -4,8 +4,30 @@ public class Leetcode278 {
         return true;
     }
     public int firstBadVersion(int n) {
-        // list starts from 1
+        if(n==1){
+            return 1;
+        }
+
         int left = 1, right = n;
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(isBadVersion(mid)){
+                // 이 중앙이 처음 잘못된 것일 수 있으므로 mid를 포함해야 함
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        // last turn: mid = left, if isBadVersion(mid) -> right = mid(=left) else -> left = mid + 1
+        return left;
+    }
+}
+
+
+/*
+int left = 1, right = n;
         while(left < right){
             int mid = left + (right-left)/2;
             if(isBadVersion(mid)){
@@ -16,5 +38,4 @@ public class Leetcode278 {
             }
         }
         return left;
-    }
-}
+ */
