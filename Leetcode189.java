109914ecff3a031673e44636fbcf770f8b6f649e
silver-whale <garlicwhale@gmail.com>
2022-05-19 20:59:17 +0900

    Leetcode 189

--- Leetcode189.java
+++ Leetcode189.java
@@ -2,12 +2,15 @@ public class Leetcode189 {
     public void rotate(int[] nums, int k) {
         int len = nums.length;
         k = k % len;
-        rotate(nums, 0, len-1);
-        rotate(nums, 0, k-1);
-        rotate(nums, k, len-1);
-
+        // reverse the array
+        rot(nums, 0, len-1);
+        // reverse the array from 0 to k-1
+        rot(nums, 0, k-1);
+        // reverse the rest array
+        rot(nums, k, len-1);
     }
-    void rotate(int[] arr, int start, int end){
+
+    void rot(int[] arr, int start, int end){
         while(start < end){
             int temp = arr[start];
             arr[start] = arr[end];
@@ -17,3 +20,6 @@ public class Leetcode189 {
         }
     }
 }
+
+// Array reverse: Use two pointer
+// Shift: Divide into three steps
