463bf67212b9dc631000e7f421802d5f0692ed1e
silver-whale <garlicwhale@gmail.com>
2022-05-21 23:43:21 +0900

    Leetcode 344

---
+++ Leetcode344.java
@@ -0,0 +1,13 @@
+public class Leetcode344 {
+    public void reverseString(char[] s) {
+        int first = 0, second = s.length-1;
+
+        while(first<second){
+            char tmp = s[first];
+            s[first] = s[second];
+            s[second] = tmp;
+            first += 1;
+            second -= 1;
+        }
+    }
+}
