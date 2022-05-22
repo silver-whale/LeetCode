ddcb899797adf58e728a96c37a81c21a3e4eaacc
silver-whale <garlicwhale@gmail.com>
2022-05-22 22:28:08 +0900

    Leetcode 557

--- Leetcode557.java
+++ Leetcode557.java
@@ -1,34 +1,21 @@
 public class Leetcode557 {
     public String reverseWords(String s) {
-        String[] words=s.split(" ");
+        // 공백 단위로 쪼개서 배열화
+        String[] words = s.split(" ");
 
+        // StringBuilder 객체 생성
         StringBuilder temp = new StringBuilder();
 
+        // 각 단어마다
         for (int i = 0; i < words.length; i++) {
+            // 1. String을 StringBuilder화해서 뒤집는다. 이후 공백 추가.
+            // 2. 해당 StringBuilder를 다시 string화 해서 추가한다.
+
+            // Stringbuilder.append는 문자열을 매개변수로 받아서 두 번 변환해주는 것
             temp.append(String.valueOf(new StringBuilder(words[i]).reverse()+" "));
         }
 
+        // trim 함수: 왼쪽, 오른쪽 공백 제거
         return temp.toString().trim();
     }
-
-    /*
-    public String reverseWords(String s) {
-        char[] str = s.toCharArray();
-        int len = s.length(), i = 0, j = 0;
-        while(i < len){
-            while(i < len && str[i] == ' ') i++;
-            j = i;
-            while(i < len && str[i] != ' ') i++;
-            reverse(str, j, i - 1);
-        }
-        return new String(str);
-    }
-    void reverse(char[] str, int s, int e){
-        while(s < e){
-            char temp = str[s];
-            str[s++] = str[e];
-            str[e--] = temp;
-        }
-    }
-    */
 }
