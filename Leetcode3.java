3369a9702c54b1f524df25d843c8a735b06ca4fe
silver-whale <garlicwhale@gmail.com>
2022-05-26 12:41:05 +0900

    Leetcode 3

--- Leetcode3.java
+++ Leetcode3.java
@@ -4,17 +4,14 @@ import java.util.Set;
 public class Leetcode3 {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
-
-        int first = 0;
+        int start = 0;
         int answer = 0;
-
-        for (int last = 0; last < s.length(); last++) {
-            while(set.contains(s.charAt(last))) {
-                set.remove(s.charAt(first));
-                first += 1;
+        for(int last = 0; last < s.length(); last++){
+            while(set.contains(s.charAt(last))){
+                set.remove(s.charAt(start++));
             }
             set.add(s.charAt(last));
-            answer = Math.max(last-first+1, answer);
+            answer = Math.max(answer, last - start + 1);
         }
         return answer;
     }
