82e373eeef033fb86baee861d724ac1f5ec6d1ec
silver-whale <garlicwhale@gmail.com>
2022-06-24 22:16:24 +0900

    Leetcode 704

--- Java/Leetcode704.java
+++ Java/Leetcode704.java
@@ -9,7 +9,8 @@ public class Leetcode704 {
         int start = 0, end = nums.length-1;
 
         while(end >= start) {
-            int mid = start + (end - start)/2;
+            // int mid = start + (end - start)/2;
+            int mid = (start + end) / 2;
             if(nums[mid] == target){
                 return mid;
             }
