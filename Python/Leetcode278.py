43664ec46d3759803046c830950152031ee67771
silver-whale <garlicwhale@gmail.com>
2022-06-25 20:55:01 +0900

    Leetcode 278

---
+++ Python/Leetcode278.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+def isBadVersion(version: int) -> bool:
+    return True
+
+class Leetcode278:
+    def firstBadVersion(self, n: int) -> int:
+        start = 1; end = n
+        
+        while(start<=end):
+            mid = (start+end)//2
+            if(isBadVersion(mid)):
+                end = mid - 1
+            else:
+                start = mid + 1
+
+        return start
+
+
