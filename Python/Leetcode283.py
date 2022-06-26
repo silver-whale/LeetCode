84d363ddfea61cbd4fec2715fdec0eb62131bbee
silver-whale <garlicwhale@gmail.com>
2022-06-26 21:34:57 +0900

    Leetcode 283

---
+++ Python/Leetcode283.py
@@ -0,0 +1,7 @@
+class Leetcode283:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
