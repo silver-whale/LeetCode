88ad1367a5b50f96d1f63b8647ea11b66b5c5436
silver-whale <garlicwhale@gmail.com>
2022-07-28 16:24:35 +0900

    Leetcode 162

---
+++ Python/Leetcode162.py
@@ -0,0 +1,15 @@
+class Leetcode162:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums)==1: return 0
+        start, end = 0, len(nums)-1
+
+        while start<end:
+            mid = (start+end)//2
+            # 증가하는 부분 -> 오른쪽에 피크가 있음 
+            if nums[mid] < nums[mid+1]:
+                # mid+1이 더 크기 때문에 start = mid + 1
+                start = mid + 1
+            # 감소하는 부분 -> 왼쪽에 피크가 있음 
+            # mid가 최댓값일 수 있음
+                end = mid
+        return start
