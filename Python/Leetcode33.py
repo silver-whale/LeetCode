06e972cd7ee11b68c6685830d9fff91d18d47a05
silver-whale <garlicwhale@gmail.com>
2022-07-28 15:33:28 +0900

    Leetcode 33

--- Python/Leetcode33.py
+++ Python/Leetcode33.py
@@ -48,3 +48,36 @@ class Leetcode33:
                         end = mid - 1
 
         return -1
+
+class practice:
+    def search2(self, nums, target):
+        if len(nums)==1: return 0 if nums[0]==target else -1
+
+        start, end = 0, len(nums)-1
+
+        while(start<end):
+            if nums[start] == target: return start
+            if nums[end] == target: return end
+
+            mid = (start + end) // 2
+
+            if nums[mid] == target: return mid
+
+            elif nums[mid] > target:
+                if nums[start] < target:
+                    end = mid - 1
+                else:
+                    if nums[start] < nums[end]:
+                        end = mid - 1
+                    else:
+                        start = mid + 1
+            else:
+                if nums[start]>target:
+                    start + mid + 1
+                else:
+                    if nums[start]<=nums[mid]:
+                        start = mid + 1
+                    else:
+                        end = mid - 1
+
+        return -1
