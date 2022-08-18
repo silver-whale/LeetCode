9b007f6e5e14f4e78005934c453c36eba94a2971
silver-whale <garlicwhale@gmail.com>
2022-08-18 21:46:56 +0900

    Leetcode 117

--- Python/Leetcode117.py
+++ Python/Leetcode117.py
@@ -1,16 +1,19 @@
 class Leetcode117:
     def connect(self, root: 'Node') -> 'Node':
-        nodes = {}
+        depth = {}
 
-        def dfs(root, pointer):
-            if not root:
-                return
-            if pointer in nodes:
-                nodes[pointer].next = root
-            nodes[pointer] = root
+        # d는 depth
+        # 만약 해당 depth의 맨 첫 노드가 입력된다면(not in depth)
+        # 키 d는 해당 노드가 먹음
+        # 이후에 입력되는 노드들은 같은 층에 있는 뒤쪽 노드들이기 때문에 뒤에 달림
+        def dfs(root, d):
+            if not root: return
+            if d in depth:
+                depth[d].next = root
+            depth[d] = root
             root.next = None
-            dfs(root.left, pointer+1)
-            dfs(root.right, pointer+1)
+            dfs(root.left, d+1)
+            dfs(root.right, d+1)
+        
         dfs(root, 0)
-
         return root
