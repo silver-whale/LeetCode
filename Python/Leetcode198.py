d23ee58ad82492dfc98aa7ce417bbd10447b468a
silver-whale <garlicwhale@gmail.com>
2022-09-04 15:34:12 +0900

    Leetcode 198

--- Python/Leetcode198.py
+++ Python/Leetcode198.py
@@ -8,3 +8,15 @@ class Leetcode198:
             prev, curr = curr, max(prev + n, curr)
         
         return curr
+
+    def dp(self, nums):
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+
+        dp = [0] * (len(nums)+1)
+        dp[1] = nums[0]
+
+        for i in range(1, len(nums)):
+            dp[i+1] = max(dp[i], dp[i-1] + nums[i])
+        
+        return dp[len(dp)-1]
