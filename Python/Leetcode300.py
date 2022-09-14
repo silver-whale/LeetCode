525af0e6624c52c4b8af6926cf9e8f368f7164f4
silver-whale <garlicwhale@gmail.com>
2022-09-14 21:22:35 +0900

    Leetcode 300

--- Python/Leetcode300.py
+++ Python/Leetcode300.py
@@ -1,14 +1,16 @@
 class Leetcode300:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        if not nums: return 0
 
         n = len(nums)
         dp = [1] * n
 
         for i in range(1, n):
             for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+
+                # 연속될 숫자일 필요 없음
+                if nums[i]>nums[j]:
+                    # 기존 값 or 이전 값 + 한 칸(nums[i])
+                    dp[i] = max(dp[i], dp[j]+1)
 
         return max(dp)
