c7c87a9965e0f744e7992fb3369b674728f8cf79
silver-whale <garlicwhale@gmail.com>
2022-10-04 23:21:37 +0900

    .

--- Python/Basic.py
+++ Python/Basic.py
@@ -1,2 +1,27 @@
-class q1:
-    
+import math
+
+class solution:
+    def q1(sides):
+        Ssides = sorted(sides)
+        print(Ssides)
+        if Ssides[0]+Ssides[1] > Ssides[2]: return 1
+        else: return 2
+
+    def q2(my_string, n):
+        answer = ''
+        for s in my_string:
+            answer += s*n
+        return answer
+
+    def q3(n):
+        sn = int(n**(1/2))
+        answer = []
+        for i in range(1, sn+1):
+            if n%i == 0:
+                answer.append(i)
+                answer.append(n/i)
+        return sorted(answer)
+
+    def q4(n):
+        if math.sqrt(n) == int(math.sqrt(n)): return 1
+        else: return 2
